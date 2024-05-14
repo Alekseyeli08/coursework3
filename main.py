e92fb6ddfc60e90e66ad8d5fb6e3300b72780b09
Aleksey Manzhikov <sanal-manzhikov@mail.ru>
2024-05-14 14:30:59 +0300

    add main

---
+++ main.py
@@ -0,0 +1,14 @@
+import json
+from functions import state_executed,sort_operations, output_format
+def main():
+    with open("operations.json", "r", encoding='utf-8') as file:
+        data = json.load(file)
+        items = state_executed(data)
+        sort_items = sort_operations(items)
+
+        for i in range(5):
+            print(output_format(sort_items[i]))
+
+
+if __name__ == "__main__":
+    main()
